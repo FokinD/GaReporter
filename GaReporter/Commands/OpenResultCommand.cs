29dd6530cb72104524be4aa903a20391004d4fc9
Dmitry Fokin <fokinsoft@gmail.com>
2015-09-06 12:02:57 +0300

    обработка ошибки при попытке открыть несуществующий файл с результат

--- GaReporter/Commands/OpenResultCommand.cs
+++ GaReporter/Commands/OpenResultCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GaReporter.Properties;
 
 namespace GaReporter
 {
@@ -7,9 +8,21 @@ namespace GaReporter
 
 		public override void Execute(object parameter)
 		{
-			//FIXME для раздела открывать файл раздела
-			System.Diagnostics.Process.Start(((RequestView)parameter).FileName);
-		}
+            //FIXME для раздела открывать файл раздела
+            var request = (RequestView)parameter;
+            try
+            {
+                System.Diagnostics.Process.Start(request.FileName);
+            }
+            catch (Exception ex) {
+                System.Windows.MessageBox
+                    .Show(string.Format("{0} {1}.\n\n{2}",
+                    Resources.RefreshErrorMessage,
+                    request.Title, ex.Message),
+                    string.Format("{0} {1}", System.Windows.Application.Current.MainWindow.Title, request.Title),
+                    System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
 
 		public override bool CanExecute(object parameter)
 		{
